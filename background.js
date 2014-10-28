a00d052001198d0a6b517aae29ee20f389d75c91
Eugene Poltorakov <jslayer@gmail.com>
2014-10-28 17:22:29 +0300

    update

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-function today(){
+function getToday(){
     var a = new Date;
 
     var b = new Date(a.getFullYear(), a.getMonth(), a.getDate());
@@ -7,6 +7,8 @@ function today(){
 }
 
 chrome.storage.sync.get('data', function(v_data){
+    v_data = v_data || {};
+
     setInterval(function(){
         chrome.tabs.query({
             active : true
@@ -14,43 +16,50 @@ chrome.storage.sync.get('data', function(v_data){
 
             tabs.forEach(function(tab){
                 chrome.tabs.sendMessage(tab.id, 'getFocus', function(response){
-                    var key;
+                    var key, today, host, focus;
 
                     if (response) {
-                        var host = response.host;
-
-                        var focus = response.focus;
+                        host = response.host;
+                        focus = response.focus;
 
                         if (focus) {
-                            key = [response.host, today()].join('_');
+                            today = getToday();
 
-                            if (!v_data[key]) {
-                                v_data[key] = 0;
+                            if (!v_data[today]) {
+                                v_data[today] = {};
                             }
 
-                            v_data[key]++;
+                            if (!v_data[today][response.host]) {
+                                v_data[today][response.host] = 0;
+                            }
+
+                            v_data[today][response.host]++;
                         }
                     }
                 });
             });
-
-            //chrome.tabs.sendMessage()
         });
     }, 1000);
 
     setInterval(function(){
         chrome.storage.sync.set(v_data, function(){
-            //console.log('saved', v_data);
-        })
+        });
     }, 10000);
 
     chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
             switch (request) {
                 case 'getData':
-                    sendResponse(v_data);
+                    sendResponse({
+                        data : v_data
+                    });
+                    break;
+                case 'clearData':
+                    chrome.storage.sync.clear(function(){
+                        v_data = {};
+                        sendResponse('ok');
+                    });
                     break;
                 default :
-                //sendResponse({})
             }
         }
     );
--- options.js
+++ options.js
@@ -1,17 +1,40 @@
 var data;
 
 angular.module('enoughApp', [])
+    .filter('bytes', function() {
+        return function(bytes, precision) {
+            if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+            if (typeof precision === 'undefined') precision = 1;
+            var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+                number = Math.floor(Math.log(bytes) / Math.log(1024));
+            return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+        }
+    })
+    .filter('humanizeDuration', function() {
+        return function(value) {
+            return humanizeDuration(value * 1000, {
+                language : 'en'
+            });
+        }
+    })
     .controller('EnoughController', ['$scope', '$interval', function($scope, $interval){
-
         $scope.data = {};
+        $scope.used = 0;
+
+        $scope.clearAction = function() {
+            $scope.data = {};
+            chrome.extension.sendRequest(null, 'clearData', function(response){});
+        };
 
         $interval(function(){
-            console.log('1123');
             chrome.extension.sendRequest(null, 'getData', function(response){
-                console.log(response);
-                $scope.data = response;
-                //$scope.apply();
+                $scope.data = response.data;
             });
+
+            chrome.storage.sync.getBytesInUse(function(used) {
+                $scope.used = used;
+            });
+
         }, 1000);
 
     }]);
