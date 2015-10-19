a93c85fe4a77b4fed3d71bc1d7a294a398618cdf
Eugene Poltorakov <jslayer@gmail.com>
2015-10-19 20:37:56 +0300

    add allow\deny buttons

--- frame.js
+++ frame.js
@@ -45,6 +45,59 @@ angular.module('FrameApp', ['UI'])
 
         $s.status = STATUSES.NA;
 
+        $s.setStatus = function(status){
+            chrome.extension.sendRequest(null, 'getTodayData', function(response){
+                var sns, _ixg, _ixb;
+
+                console.log(response);
+
+                if (response.data) {
+                    sns = {
+                        gs : response.data.gs,
+                        bs : response.data.bs
+                    };
+
+                    _ixg = sns.gs.indexOf(host);
+                    _ixb = sns.bs.indexOf(host);
+
+                    switch (status) {
+                        case STATUSES.ALLOW:
+                            if (_ixg === -1) {
+                                sns.gs.push(host);
+                            }
+                            if(_ixb !== -1) {
+                                sns.bs.splice(_ixb, 1);
+                            }
+                            break;
+                        case STATUSES.DENY:
+                            if (_ixb === -1) {
+                                sns.bs.push(host);
+                            }
+                            if(_ixg !== -1) {
+                                sns.gs.splice(_ixb, 1);
+                            }
+                            break;
+                    }
+
+                    chrome.extension.sendRequest(null, ({
+                        request : 'saveBs',
+                        data    : sns.bs
+                    }), function(){
+                    });
+
+                    chrome.extension.sendRequest(null, ({
+                        request : 'saveGs',
+                        data    : sns.gs
+                    }), function(){
+                    });
+
+                    $s.$apply(function(){
+                        $s.status = status;
+                    });
+                }
+            });
+        };
+
         $i(function(){
             chrome.extension.sendRequest(null, 'getTodayData', function(response){
                 if (response && response.today){
@@ -78,9 +131,9 @@ angular.module('FrameApp', ['UI'])
                 _p = (_val - _val % MV[i]) / MV[i];
                 _val -= MV[i] * _p;
 
-                if (_p > 0){
-                    parts.push(_p < 10 ? '0' + _p : _p);
-                }
+                //if (_p > 0){
+                parts.push(_p < 10 ? '0' + _p : _p);
+                //}
             }
 
             return parts.join(' : ');
