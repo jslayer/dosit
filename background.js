a60cb7c7fc429dcbafcfa680d2b677708c212884
Eugene Poltorakov <jslayer@gmail.com>
2015-10-21 01:11:18 +0300

    working on new data structure

--- background.js
+++ background.js
@@ -1,3 +1,33 @@
+/*
+ *
+ * Storage
+ *
+ * d : {uid:[domain, value, status]}
+ *
+ * Example:
+ *
+ * {
+ *    domains : {
+ *        55 : ['chrome.com' : 2]
+ *    },
+ *    data : {
+ *        DDMMYYYY : {
+ *            55 : 2
+ *        }
+ *    }
+ *    uid : 0
+ * }
+ *
+ */
+
+var STATUSES = {
+    NA      : 0,
+    UNKNOWN : 1,
+    DENY    : 2,
+    ALLOW   : 3
+};
+
+
 function get_today(){
     var a;
 
@@ -33,16 +63,16 @@ var Storage = (function(){
                         v_data = {};
                     }
 
-                    if (!v_data.data){
-                        v_data.data = {};
+                    if (!v_data.domains) {
+                        v_data.domains = {};
                     }
 
-                    if (!v_data.bs){
-                        v_data.bs = [];
+                    if (typeof v_data.uid !== 'number') {
+                        v_data.uid = 0;
                     }
 
-                    if (!v_data.gs){
-                        v_data.gs = [];
+                    if (!v_data.val) {
+                        v_data.val = {};
                     }
 
                     Storage.setData(v_data);
@@ -50,7 +80,71 @@ var Storage = (function(){
             }
         },
 
+        getUid : function(host){
+            var i, uid;
+
+            uid = false;
+
+            if (data){
+                for (i in data.domains) {
+                    if (data.domains.hasOwnProperty(i) && data.domains[i][0] === host){
+                        uid = i;
+                        break;
+                    }
+                }
+            }
+
+            if (uid === false){
+                uid = data.uid++;
+                data.domains[uid] = [host, STATUSES.UNKNOWN];
+            }
+
+            return uid;
+        },
+
+        incHost : function(host, date){
+            var uid;
+
+            console.log(arguments, 'INC');
+
+            uid = obj.getUid(host);
+
+            if (!data.val[date]){
+                data.val[date] = {};
+            }
+
+            if (!data.val[date][uid]){
+                data.val[date][uid] = 0;
+            }
+
+            data.val[date][uid]++;
+        },
+
+        setStatus : function(host, status){
+            data.domains[obj.getUid(host)][1] = status;
+        },
+
+        getStatus : function(host){
+            return data.domains[obj.getUid(host)][1];
+        },
+
+        getByDay : function(host, date){
+            var uid = obj.getUid(host);
+
+            var status = obj.getStatus(host);
+
+            console.log(arguments, uid, status);
+
+            var day = data.val[date] && data.val[date][uid] ? data.val[date][uid] : false;
+
+            return {
+                status : status,
+                data   : day
+            }
+        },
+
         setData : function(value){
+            //console.log('set data');
             data = value;
 
             if (ready){
@@ -81,40 +175,15 @@ Storage.getData(function(data){
         chrome.tabs.query({
             active : true
         }, function(tabs){
-
             if (tabs.length > 0){
-                Storage.getData(function(data){
-                    tabs.forEach(function(tab){
-                        chrome.tabs.sendMessage(tab.id, 'getFocus', function(response){
-                            var key, today, host, focus;
+                tabs.forEach(function(tab){
+                    chrome.tabs.sendMessage(tab.id, 'getFocus', function(response){
+                        var key, today, host, focus;
 
-                            if (response){
-                                host = response.host;
-                                focus = response.focus;
-
-                                if (focus){
-                                    today = get_today();
-
-                                    if (!data.data[today]){
-                                        data.data[today] = {};
-                                    }
-
-                                    if (!data.data[today][response.host]){
-                                        data.data[today][response.host] = 0;
-                                    }
-
-                                    data.data[today][response.host]++;
-                                }
-
-                                chrome.browserAction.setIcon({
-                                    path  : data.bs.indexOf(host) > -1 ? 'icon128_4.png' : 'icon128.png',
-                                    tabId : tab.id
-                                }, function(){
-                                });
-                            }
-
-                        });
-                    })
+                        if (response && response.focus){
+                            Storage.incHost(response.host, get_today());
+                        }
+                    });
                 });
             }
         });
@@ -127,59 +196,73 @@ Storage.getData(function(data){
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
         switch (request) {
-            case 'getData':
-                Storage.getData(function(data){
-                    sendResponse(data);
-                });
-                break;
+            //case 'getData':
+            //    Storage.getData(function(data){
+            //        sendResponse(data);
+            //    });
+            //    break;
             case 'clearData':
                 chrome.storage.sync.clear(function(){
                     Storage.setData({
-                        data : {},
-                        bs   : [],
-                        gs   : []
+                        domains : {},
+                        val    : {},
+                        uid     : 0
                     });
                     sendResponse('ok');
                 });
                 break;
-            case 'getTodayData':
-                Storage.getData(function(data){
-                    var result, today;
-
-                    today = get_today();
-
-                    if (data && data.data && data.data[today]) {
-                        result = {
-                            today : data.data[today],
-                            data  : data
-                        };
-                    }
-
-                    sendResponse(result);
-                });
+            case 'getHostToday':
+                console.log(request);
                 break;
+            //case 'getTodayData':
+            //    Storage.getData(function(data){
+            //        var result, today;
+            //
+            //        today = get_today();
+            //
+            //        if (data && data.val && data.val[today]) {
+            //            result = {
+            //                today : data.val[today],
+            //                data  : data
+            //            };
+            //        }
+            //
+            //        sendResponse(result);
+            //    });
+            //    break;
             default :
-                if (request && request.request && request.data){
-                    Storage.getData(function(data){
-                        switch (request.request) {
-                            case 'saveBs':
-                                data.bs = request.data;
-                                Storage.setData(data);
-                                //saveData(v_data);
-                                break;
+                if (request){
+                    switch (request.type) {
+                        case 'getTodayData':
+                            var bd = Storage.getByDay(request.host, get_today());
+                            sendResponse(bd);
+                            break;
+                        default:
+                            if (request.request && request.data){
+                                Storage.getData(function(data){
+                                    switch (request.request) {
+                                        case 'saveBs':
+                                            data.bs = request.data;
+                                            Storage.setData(data);
+                                            //saveData(v_data);
+                                            break;
 
-                            case 'saveGs':
-                                data.gs = request.data;
-                                Storage.setData(data);
-                                break;
+                                        case 'saveGs':
+                                            data.gs = request.data;
+                                            Storage.setData(data);
+                                            break;
 
-                            case 'saveGBs':
-                                data.gs = request.data.gs;
-                                data.bs = request.data.bs;
-                                Storage.setData(data);
-                                break;
-                        }
-                    });
+                                        case 'saveGBs':
+                                            data.gs = request.data.gs;
+                                            data.bs = request.data.bs;
+                                            Storage.setData(data);
+                                            break;
+                                    }
+                                });
+                            }
+
+                    }
+                    ;
                 }
         }
     }
--- frame.js
+++ frame.js
@@ -45,77 +45,86 @@ angular.module('FrameApp', ['UI'])
         $s.status = STATUSES.NA;
 
         $s.setStatus = function(status){
-            chrome.extension.sendRequest(null, 'getTodayData', function(response){
-                var sns, _ixg, _ixb;
-
-                if (response.data){
-                    sns = {
-                        gs : response.data.gs,
-                        bs : response.data.bs
-                    };
-
-                    _ixg = sns.gs.indexOf(host);
-                    _ixb = sns.bs.indexOf(host);
-
-                    switch (status) {
-                        case STATUSES.ALLOW:
-                            if (_ixg === -1){
-                                sns.gs.push(host);
-                            }
-                            if (_ixb !== -1){
-                                sns.bs.splice(_ixb, 1);
-                            }
-                            break;
-                        case STATUSES.DENY:
-                            if (_ixb === -1){
-                                sns.bs.push(host);
-                            }
-                            if (_ixg !== -1){
-                                sns.gs.splice(_ixb, 1);
-                            }
-                            break;
-                    }
-
-                    chrome.extension.sendRequest(null, ({
-                        request : 'saveGBs',
-                        data    : {
-                            bs : sns.bs,
-                            gs : sns.gs
-                        }
-                    }), function(){
-                    });
-                    //
-                    //chrome.extension.sendRequest(null, ({
-                    //    request : 'saveGs',
-                    //    data    : sns.gs
-                    //}), function(){
-                    //});
-
-                    $s.$apply(function(){
-                        $s.status = status;
-                    });
-                }
-            });
+            //chrome.extension.sendRequest(null, 'getTodayData', function(response){
+            //    var sns, _ixg, _ixb;
+            //
+            //    if (response.data){
+            //        sns = {
+            //            gs : response.data.gs,
+            //            bs : response.data.bs
+            //        };
+            //
+            //        _ixg = sns.gs.indexOf(host);
+            //        _ixb = sns.bs.indexOf(host);
+            //
+            //        switch (status) {
+            //            case STATUSES.ALLOW:
+            //                if (_ixg === -1){
+            //                    sns.gs.push(host);
+            //                }
+            //                if (_ixb !== -1){
+            //                    sns.bs.splice(_ixb, 1);
+            //                }
+            //                break;
+            //            case STATUSES.DENY:
+            //                if (_ixb === -1){
+            //                    sns.bs.push(host);
+            //                }
+            //                if (_ixg !== -1){
+            //                    sns.gs.splice(_ixb, 1);
+            //                }
+            //                break;
+            //        }
+            //
+            //        chrome.extension.sendRequest(null, ({
+            //            request : 'saveGBs',
+            //            data    : {
+            //                bs : sns.bs,
+            //                gs : sns.gs
+            //            }
+            //        }), function(){
+            //        });
+            //        //
+            //        //chrome.extension.sendRequest(null, ({
+            //        //    request : 'saveGs',
+            //        //    data    : sns.gs
+            //        //}), function(){
+            //        //});
+            //
+            //        $s.$apply(function(){
+            //            $s.status = status;
+            //        });
+            //    }
+            //});
         };
 
         $i(function(){
-            chrome.extension.sendRequest(null, 'getTodayData', function(response){
-                if (response && response.today){
-                    $s.$apply(function(){
-                        switch (true) {
-                            case response.data.gs.indexOf(host) > -1:
-                                $s.status = STATUSES.ALLOW;
-                                break;
-                            case response.data.bs.indexOf(host) > -1:
-                                $s.status = STATUSES.DENY;
-                                break;
-                            default:
-                                $s.status = STATUSES.UNKNOWN;
-                        }
-                        $s.c = response.today[host] || 0;
-                    });
-                }
+            chrome.extension.sendRequest(null, {
+                type : 'getTodayData',
+                host : get_host()
+            }, function(response){
+                $s.$apply(function(){
+                    $s.status = response.status;
+                    $s.c = response.data || 0;
+                });
             });
+            //chrome.extension.sendRequest(null, 'getTodayData', function(response){
+            //    if (response && response.today){
+            //        $s.$apply(function(){
+            //            switch (true) {
+            //                case response.data.gs.indexOf(host) > -1:
+            //                    $s.status = STATUSES.ALLOW;
+            //                    break;
+            //                case response.data.bs.indexOf(host) > -1:
+            //                    $s.status = STATUSES.DENY;
+            //                    break;
+            //                default:
+            //                    $s.status = STATUSES.UNKNOWN;
+            //            }
+            //            $s.c = response.today[host] || 0;
+            //        });
+            //    }
+            //});
         }, 1000);
     }])
     .filter('dn', function(){
